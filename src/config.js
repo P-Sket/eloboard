802076b7b2ba60289e63b5e636ac9c694fd77581
P-Sket <aihub.anonymous@gmail.com>
2026-07-31 02:41:58 +0900

    Delete src directory

--- src/config.js
+++
@@ -1,8 +0,0 @@
-export const CONFIG = {
-  githubOwner: "",
-  githubRepo: "",
-  googleSpreadsheetUrl: "https://docs.google.com/spreadsheets/d/1yYgtArH80b3jl7vKc8m9k-WGAuIly8v3RGCp3-X3rwc/edit?usp=sharing",
-  googleSheetsWebAppUrl: "https://script.google.com/macros/s/AKfycbzHsD8E9kjQGZml2rf2nKwy_AiOCqb6ax3OgTVE0sFraoi3U8lV1JupJKnZNeF3_0CNbQ/exec",
-  dataSource: "google-sheets",
-  autoApproveNormalMatches: true
-};
